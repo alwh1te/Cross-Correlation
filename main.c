5eb7171d946e0b745ec83227730f36de61fdb39f
alw <alexander.s.filatov@gmail.com>
2024-04-13 15:25:37 +0300

    idk but there is an ans

--- main.c
+++ main.c
@@ -1,176 +1,155 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <fftw3.h>
-#include <libavdevice/avdevice.h>
-#include <libavformat/avformat.h>
-#include <libavcodec/avfft.h>
-#include <libavutil/audio_fifo.h>
 #include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/opt.h>
 
-#define MAX_AUDIO_FRAME_SIZE 192000
+#define MAX_FRAME_SIZE 192000 // Максимальный размер фрейма аудио (для максимальной продолжительности сегмента)
 
-
-void load_audio_data(const char *filename, float *audio_data, int length) {
-    AVFormatContext *format_context = NULL;
-    AVCodecContext *codec_context = NULL;
-    AVCodec *codec = NULL;
-    AVPacket packet;
-    AVFrame *frame = NULL;
-    int stream_index = -1;
-    int got_frame = 0;
-    int audio_frame_size = 0;
-    int total_samples = 0;
-    int sample_size = av_get_bytes_per_sample(AV_SAMPLE_FMT_FLT); // Size of one sample in bytes
-    int sample_count = length / sample_size; // Number of samples to read
-
-//    av_register_all();
-    avformat_network_init();
-
-    // Open the input file
-    if (avformat_open_input(&format_context, filename, NULL, NULL) < 0) {
-        fprintf(stderr, "Error: Could not open file '%s'\n", filename);
-        return;
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Использование: %s <файл_1> <файл_2>\n", argv[0]);
+        return 1;
     }
 
-    // Find stream information
-    if (avformat_find_stream_info(format_context, NULL) < 0) {
-        fprintf(stderr, "Error: Could not find stream information\n");
-        avformat_close_input(&format_context);
-        return;
+    const char *filename1 = argv[1];
+    const char *filename2 = argv[2];
+
+
+    // Открытие файлов
+    AVFormatContext *formatCtx1 = NULL, *formatCtx2 = NULL;
+    if (avformat_open_input(&formatCtx1, filename1, NULL, NULL) != 0 ||
+        avformat_find_stream_info(formatCtx1, NULL) < 0) {
+        printf("Ошибка при открытии файла 1\n");
+        return 1;
+    }
+    if (avformat_open_input(&formatCtx2, filename2, NULL, NULL) != 0 ||
+        avformat_find_stream_info(formatCtx2, NULL) < 0) {
+        printf("Ошибка при открытии файла 2\n");
+        return 1;
     }
 
-    // Find the audio stream
-    for (int i = 0; i < format_context->nb_streams; i++) {
-        if (format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-            stream_index = i;
+    // Получение аудио-стримов
+    int audioStreamIndex1 = -1, audioStreamIndex2 = -1;
+    for (int i = 0; i < formatCtx1->nb_streams; i++) {
+        if (formatCtx1->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            audioStreamIndex1 = i;
             break;
         }
     }
-
-    if (stream_index == -1) {
-        fprintf(stderr, "Error: Could not find audio stream in the input file\n");
-        avformat_close_input(&format_context);
-        return;
+    for (int i = 0; i < formatCtx2->nb_streams; i++) {
+        if (formatCtx2->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            audioStreamIndex2 = i;
+            break;
+        }
+    }
+    if (audioStreamIndex1 == -1 || audioStreamIndex2 == -1) {
+        printf("Аудио-стрим не найден в одном из файлов\n");
+        return 1;
     }
 
-    // Get a pointer to the codec context for the audio stream
-    codec_context = avcodec_alloc_context3(NULL);
-    avcodec_parameters_to_context(codec_context, format_context->streams[stream_index]->codecpar);
-
-    // Find the decoder for the audio stream
-    codec = avcodec_find_decoder(codec_context->codec_id);
-    if (!codec) {
-        fprintf(stderr, "Error: Unsupported codec\n");
-        avcodec_free_context(&codec_context);
-        avformat_close_input(&format_context);
-        return;
+    // Получение декодеров
+    AVCodec *codec1 = avcodec_find_decoder(formatCtx1->streams[audioStreamIndex1]->codecpar->codec_id);
+    AVCodec *codec2 = avcodec_find_decoder(formatCtx2->streams[audioStreamIndex2]->codecpar->codec_id);
+    if (!codec1 || !codec2) {
+        printf("Декодер не найден\n");
+        return 1;
+    }
+    AVCodecContext *codecCtx1 = avcodec_alloc_context3(codec1);
+    AVCodecContext *codecCtx2 = avcodec_alloc_context3(codec2);
+    avcodec_parameters_to_context(codecCtx1, formatCtx1->streams[audioStreamIndex1]->codecpar);
+    avcodec_parameters_to_context(codecCtx2, formatCtx2->streams[audioStreamIndex2]->codecpar);
+    if (avcodec_open2(codecCtx1, codec1, NULL) < 0 || avcodec_open2(codecCtx2, codec2, NULL) < 0) {
+        printf("Не удалось открыть декодер\n");
+        return 1;
     }
 
-    // Open codec
-    if (avcodec_open2(codec_context, codec, NULL) < 0) {
-        fprintf(stderr, "Error: Could not open codec\n");
-        avcodec_free_context(&codec_context);
-        avformat_close_input(&format_context);
-        return;
-    }
+    // Инициализация FFTW
+    fftw_plan plan1, plan2, plan3;
+    fftw_complex *in1, *in2, *out1, *out2, *corr;
+    in1 = (fftw_complex *) fftw_malloc(MAX_FRAME_SIZE * sizeof(fftw_complex));
+    in2 = (fftw_complex *) fftw_malloc(MAX_FRAME_SIZE * sizeof(fftw_complex));
+    out1 = (fftw_complex *) fftw_malloc(MAX_FRAME_SIZE * sizeof(fftw_complex));
+    out2 = (fftw_complex *) fftw_malloc(MAX_FRAME_SIZE * sizeof(fftw_complex));
+    corr = (fftw_complex *) fftw_malloc(MAX_FRAME_SIZE * sizeof(fftw_complex));
+    plan1 = fftw_plan_dft_1d(MAX_FRAME_SIZE, in1, out1, FFTW_FORWARD, FFTW_ESTIMATE);
+    plan2 = fftw_plan_dft_1d(MAX_FRAME_SIZE, in2, out2, FFTW_FORWARD, FFTW_ESTIMATE);
+    plan3 = fftw_plan_dft_1d(MAX_FRAME_SIZE, corr, corr, FFTW_BACKWARD, FFTW_ESTIMATE);
 
-    // Allocate audio frame
-    frame = av_frame_alloc();
-    if (!frame) {
-        fprintf(stderr, "Error: Could not allocate audio frame\n");
-        avcodec_free_context(&codec_context);
-        avformat_close_input(&format_context);
-        return;
-    }
-
-    // Initialize packet
-    av_init_packet(&packet);
-    packet.data = NULL;
-    packet.size = 0;
-
-    // Read frames and store audio data
-    while (av_read_frame(format_context, &packet) >= 0) {
-        if (packet.stream_index == stream_index) {
-            int ret = avcodec_send_packet(codec_context, &packet);
-            if (ret < 0) {
-                fprintf(stderr, "Error: Error sending a packet for decoding\n");
-                break;
-            }
-
-            while (ret >= 0) {
-                ret = avcodec_receive_frame(codec_context, frame);
-                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-                    break;
-                else if (ret < 0) {
-                    fprintf(stderr, "Error: Error during decoding\n");
-                    break;
-                }
-
-                // Convert audio frame to float
+    // Считывание и обработка аудио-данных
+    AVPacket packet;
+    int frameFinished;
+    while (av_read_frame(formatCtx1, &packet) >= 0) {
+        if (packet.stream_index == audioStreamIndex1) {
+            AVFrame *frame = av_frame_alloc();
+            avcodec_send_packet(codecCtx1, &packet);
+            if (avcodec_receive_frame(codecCtx1, frame) == 0) {
                 for (int i = 0; i < frame->nb_samples; i++) {
-                    for (int j = 0; j < codec_context->channels; j++) {
-                        if (total_samples < length) {
-                            audio_data[total_samples] = (float)frame->data[j][i] / INT16_MAX;
-                            total_samples++;
-                        } else {
-                            break;
-                        }
-                    }
+                    in1[i][0] = frame->data[0][i];
+                    in1[i][1] = 0.0;
                 }
+                fftw_execute(plan1);
             }
+            av_frame_free(&frame);
+            break;
+        }
+        av_packet_unref(&packet);
+    }
+    while (av_read_frame(formatCtx2, &packet) >= 0) {
+        if (packet.stream_index == audioStreamIndex2) {
+            AVFrame *frame = av_frame_alloc();
+            avcodec_send_packet(codecCtx2, &packet);
+            if (avcodec_receive_frame(codecCtx2, frame) == 0) {
+                for (int i = 0; i < frame->nb_samples; i++) {
+                    in2[i][0] = frame->data[0][i];
+                    in2[i][1] = 0.0;
+                }
+                fftw_execute(plan2);
+            }
+            av_frame_free(&frame);
+            break;
         }
         av_packet_unref(&packet);
     }
 
-    // Clean up
-    av_frame_free(&frame);
-    avcodec_free_context(&codec_context);
-    avformat_close_input(&format_context);
-}
-
-int main(int argc, char *argv[]) {
-
-    if (argc < 2) {
-        printf("Usage: %s <file_one>\n", argv[0]);
-        return 1;
+    // Кросс-корреляция
+    for (int i = 0; i < MAX_FRAME_SIZE; i++) {
+        corr[i][0] = (out1[i][0] * out2[i][0] + out1[i][1] * out2[i][1]) / MAX_FRAME_SIZE;
+        corr[i][1] = (out1[i][1] * out2[i][0] - out1[i][0] * out2[i][1]) / MAX_FRAME_SIZE;
     }
-
-    const char *file_one = argv[1];
-    char *file_two = NULL;
-    if (argc == 3) {
-        file_two = argv[2];
-    }
-//    int64_t ff_(const double *x, const double *y, int n);
-
-    AVFormatContext *fmt_ctx = NULL;
-
-    if (avformat_open_input(&fmt_ctx, file_one, NULL, NULL) != 0) {
-        fprintf(stderr, "Could not open file: %s\n", file_one);
-        return 1;
-    }
-
-    if (avformat_find_stream_info(fmt_ctx, NULL) < 0) {
-        fprintf(stderr, "Could not find stream information\n");
-        return 1;
-    }
-
-    int audio_stream_index = -1;
-    for (int i = 0; i < fmt_ctx->nb_streams; i++) {
-        if (fmt_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-            audio_stream_index = i;
-            break;
+    fftw_execute(plan3);
+    double max_corr = 0.0;
+    int max_index = 0;
+    for (int i = 0; i < MAX_FRAME_SIZE; i++) {
+        double abs_corr = sqrt(corr[i][0] * corr[i][0] + corr[i][1] * corr[i][1]);
+        if (abs_corr > max_corr) {
+            max_corr = abs_corr;
+            max_index = i;
         }
     }
 
-    if (audio_stream_index == -1) {
-        fprintf(stderr, "No audio stream found in the input file\n");
-        return 1;
-    }
-//    int max = calculate_cross_correlation(fmt_ctx, );
-    AVCodecParameters *codec_params = fmt_ctx->streams[audio_stream_index]->codecpar;
-    int sample_rate = codec_params->sample_rate;
+    // Вычисление временного смещения
+    double sample_rate = formatCtx1->streams[audioStreamIndex1]->codecpar->sample_rate;
+    double time_shift = (double)max_index / sample_rate;
 
-    printf("Sample rate: %d Hz\n", sample_rate);
+    // Вывод временного смещения
+    printf("Временное смещение: %f секунд\n", time_shift);
 
-    avformat_close_input(&fmt_ctx);
+    // Освобождение ресурсов
+    fftw_destroy_plan(plan1);
+    fftw_destroy_plan(plan2);
+    fftw_destroy_plan(plan3);
+    fftw_free(in1);
+    fftw_free(in2);
+    fftw_free(out1);
+    fftw_free(out2);
+    fftw_free(corr);
+    avformat_close_input(&formatCtx1);
+    avformat_close_input(&formatCtx2);
+    avcodec_free_context(&codecCtx1);
+    avcodec_free_context(&codecCtx2);
+
     return 0;
 }
